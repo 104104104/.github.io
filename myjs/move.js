0abe6af8e240a678730a8b64d97f68227672be3e
{104104104} <{rz344788@yahoo.co.jp}>
2019-09-05 15:16:01 +0900

    微修正

--- myjs/move.js
+++ myjs/move.js
@@ -1,44 +1,52 @@
+//NowLoadingのプラグイン
+Pace.on('done', function () {
+	$('.wrap').addClass('do');
+	$('.wrap').hide().fadeIn();
+});
+//NowLoadingここまで
+
 //マップをレスポンシブにするためのjqueryプラグイン
-$(function() {
-    $('img[usemap]').rwdImageMaps();
+$(function () {
+	$('img[usemap]').rwdImageMaps();
 });
+//マップレスポンシブここまで
 
-var posi_top,wih_half,current_view;
+var posi_top, wih_half, current_view;
 
-$(window).on('load',function(){
+$(window).on('load', function () {
 	var wih = window.innerHeight;
-	var wih_half = wih/2;
+	var wih_half = wih / 2;
 	current_view = wih_half;
 	set_posi();
 });
 
-$(window).resize(function() {
+$(window).resize(function () {
 
 	var wih = window.innerHeight;
-	var wih_half = wih/2;
+	var wih_half = wih / 2;
 	current_view = wih_half;
 	set_posi();
 });
 
-$(window).scroll(function(){
+$(window).scroll(function () {
 
 	var wih = window.innerHeight;
-	var wih_half = wih/2;
-    current_view = $(this).scrollTop();
+	var wih_half = wih / 2;
+	current_view = $(this).scrollTop();
 	set_posi();
 
 });
 
-function set_posi(){
+function set_posi() {
 
-	$('.notAbsolute').each(function() {
-        var posi = $(this).offset();
+	$('.notAbsolute').each(function () {
+		var posi = $(this).offset();
 		posi_top = posi.top;
 
-        //戻ればなんども
-		if(current_view > posi_top+this.height*(8/9)){
+		//戻ればなんども
+		if (current_view > posi_top + this.height * (8 / 9)) {
 			$('.fixed-menu').addClass('do');
-		}else{
+		} else {
 			$('.fixed-menu').removeClass('do');
 		}
 	});
